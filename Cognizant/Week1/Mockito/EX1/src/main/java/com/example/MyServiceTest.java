8e525d55cec48adbf68e70aa94e5ea0c5d1d3723
Kasak Singh <kasaksingh81@gmail.com>
2026-07-03 11:31:52 +0530

    Create MyServiceTest.java

---
+++ Cognizant/Week1/Mockito/EX1/src/main/java/com/example/MyServiceTest.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class MyServiceTest {
+
+    @Test
+    public void testExternalApi() {
+
+        // Step 1: Create mock object
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        // Step 2: Stub the method
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+        // Step 3: Inject mock into service
+        MyService service = new MyService(mockApi);
+
+        // Act
+        String result = service.fetchData();
+
+        // Assert
+        assertEquals("Mock Data", result);
+    }
+}
