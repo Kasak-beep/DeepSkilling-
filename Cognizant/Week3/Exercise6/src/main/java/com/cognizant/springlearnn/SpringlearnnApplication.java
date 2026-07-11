8e098bf38c128e2ddde82d64bb5d0fde0a3448a2
Kasak Singh <kasaksingh81@gmail.com>
2026-07-11 09:41:10 +0530

    Add files via upload

---
+++ Cognizant/Week3/Exercise6/src/main/java/com/cognizant/springlearnn/SpringlearnnApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearnn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlearnnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlearnnApplication.class, args);
+	}
+
+}
---
+++ Cognizant/Week3/Exercise6/src/test/java/com/cognizant/springlearnn/SpringlearnnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearnn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlearnnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
