4b39dc4c15124b7b1c233ebb0f8738600b35b8c3
Kasak Singh <kasaksingh81@gmail.com>
2026-07-18 01:28:41 +0530

    .

---
+++ Cognizant/Week5/Exercise5/cohort-dashboard/src/cohorts.js
@@ -0,0 +1,28 @@
+const cohorts = [
+  {
+    id: 1,
+    name: "React Training",
+    status: "Ongoing",
+    startDate: "10-Jan-2026",
+    coach: "Rahul",
+    trainer: "Amit"
+  },
+  {
+    id: 2,
+    name: "Java Full Stack",
+    status: "Completed",
+    startDate: "15-Dec-2025",
+    coach: "Priya",
+    trainer: "Neha"
+  },
+  {
+    id: 3,
+    name: "Python Bootcamp",
+    status: "Ongoing",
+    startDate: "05-Feb-2026",
+    coach: "Ankit",
+    trainer: "Rohit"
+  }
+];
+
+export default cohorts;
