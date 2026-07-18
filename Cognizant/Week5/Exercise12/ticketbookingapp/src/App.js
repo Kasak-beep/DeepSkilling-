c785a2180abd3b0f69ded97d5f49df6740c878c3
Kasak Singh <kasaksingh81@gmail.com>
2026-07-18 09:52:52 +0530

    .

---
+++ Cognizant/Week5/Exercise12/ticketbookingapp/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Guest from "./Components/Guest";
+import User from "./Components/User";
+
+function App() {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <div style={{ margin: "20px" }}>
+
+            <h1>Ticket Booking App</h1>
+
+            {
+                isLoggedIn ?
+                <button onClick={() => setIsLoggedIn(false)}>
+                    Logout
+                </button>
+                :
+                <button onClick={() => setIsLoggedIn(true)}>
+                    Login
+                </button>
+            }
+
+            <hr />
+
+            {
+                isLoggedIn ? <User /> : <Guest />
+            }
+
+        </div>
+    );
+}
+
+export default App;
