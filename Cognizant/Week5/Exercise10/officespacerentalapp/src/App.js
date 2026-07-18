651155e2a61ed527729d801e7350adc6a2b1e9d8
Kasak Singh <kasaksingh81@gmail.com>
2026-07-18 09:10:30 +0530

    .

---
+++ Cognizant/Week5/Exercise10/officespacerentalapp/src/App.js
@@ -0,0 +1,17 @@
+body{
+    font-family: Arial;
+    margin:20px;
+}
+
+img{
+    width:300px;
+    height:200px;
+    border-radius:10px;
+}
+
+.office{
+    border:1px solid black;
+    padding:15px;
+    margin:15px;
+    width:350px;
+}
